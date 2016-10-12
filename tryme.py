efc85f552ea6f99002cdc5f4c1fd315cbc1c638b
AnnieNaosekpam <annie.naosekpam@gmail.com>
2016-10-12 00:18:04 -0700

    First check in

---
+++ tryme.py
@@ -0,0 +1,74 @@
+'''def new_line():
+    print
+
+def three_lines():
+    new_line()
+    new_line()
+    new_line()
+
+print "First line"
+three_lines()
+print "Second Line"
+
+def print_twice(param):
+    print param, param
+
+print_twice(1) '''
+
+'''def three_lines():
+    print
+
+def nine_lines():
+    three_lines()
+    three_lines()
+    three_lines()'''
+
+'''def count(n):
+    while n > 0:
+        print n
+        n = n - 1
+print "Blast"
+
+count(3)'''
+
+'''def num_digits(n):
+    count = 0
+    while n:
+        count = count + 1
+        n = n / 10
+    return count
+
+
+num_digits(710)'''
+
+
+sentence = "produces" + "\n" + "this" + "\n" + "output."
+print sentence
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
